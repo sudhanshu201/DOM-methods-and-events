4c6b42f003519809b96958734aa3d18f6a459330
sudhanshu201 <113603064+sudhanshu201@users.noreply.github.com>
2023-07-19 22:50:40 +0530

    dice

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let playerName = document.querySelectorAll("input") //it returns all input tags in form of an array. further we have to access particular tag  by having indexing of this array
 
 //select all buttons
@@ -28,7 +6,12 @@ let btns = document.getElementsByClassName("diceRollButton")
 
 //select scores
 let scores = document.getElementsByTagName("span")
+
+let winnerbtn = document.getElementById("winner")
+
+let output = document.querySelector(".output")
  
+winnerbtn.disabled = true
 //events, addEventListner
 
 for (let i = 0; i < btns.length; i++) {
@@ -38,9 +21,16 @@ for (let i = 0; i < btns.length; i++) {
     
 }
 
+let btn_click = 0
+
 function rollDice(btn_id) //this help me to understand which btn is pressed
 {
     //console.log(btn_id);
+    btn_click++
+
+    if(btn_click == 5){
+        winnerbtn.disabled = false
+    }
     btns[btn_id].disabled = true
     let arr = ["1","2","3","4","5","6"]
     let randomIndex = Math.floor(Math.random()*arr.length)  //0-6 6 is not included
@@ -48,4 +38,37 @@ function rollDice(btn_id) //this help me to understand which btn is pressed
     let randomDice = arr[randomIndex];
     //console.log(randomDice)
     scores[btn_id].innerText = randomDice    //applying innerText method in span tag
+
 }
+
+winnerbtn.addEventListener("click", decideWinner)
+
+function decideWinner(){
+    //highest score
+
+    let big = 0
+    for(let t of scores){
+        if(t.innerText > big){
+            big = t.innerText
+        }
+    }
+
+    //find all those who have highest score:
+    let winnerList = []
+
+    for(let i=0; i<=scores.length-1; i++){
+        if(scores[i ].innerText == big){
+            winnerList.push(i)
+        }
+    }
+
+    let names = []
+    for(let t of winnerList){
+       names.push( playerName[t].value )
+    }
+
+    output.innerText = names + " " + "won the game"
+}
+
+
+
